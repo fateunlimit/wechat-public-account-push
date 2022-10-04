34f49aabd972aaf87a3133684467239f010c87c1
wavyflow <987010051@qq.com>
2022-10-04 22:01:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '瑞云小可爱',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'obPbH5vfIe1hiKBpKAYxf2bUAKhY',
+      id: 'obPbH5oqNplCzuWUi6yt-mmxQidE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'uIkMUIPboUQf8beaEaP_qN8uI1_1LzuyE8pKXIRb-9Q',
+      useTemplateId: 'd6RDGMgW3Br-W1nc4XgqpRWEkY-qrezhtqhSsnlPZaY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-03',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'uIkMUIPboUQf8beaEaP_qN8uI1_1LzuyE8pKXIRb-9Q',
+  CALLBACK_TEMPLATE_ID: 'd6RDGMgW3Br-W1nc4XgqpRWEkY-qrezhtqhSsnlPZaY',
 
   CALLBACK_USERS: [
     {
