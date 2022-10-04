355e15358c0919d57294d1d834b28138d597818f
wavyflow <987010051@qq.com>
2022-10-04 21:29:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'obPbH5vwytl_qCN3w-t1DWm5Lkz0',
+      name: 'Wavy',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'obPbH5vwytl_qCN3w-t1DWm5Lkz0',
     }
   ],
 
